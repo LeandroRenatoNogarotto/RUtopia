58a19995ae2a041efe8ef4e5e0c57d8939ae4d4e
Leandro Nogarotto <leandronogarotto@alunos.utfpr.edu.br>
2023-10-07 22:14:57 -0300

    Adicionando pagina Login

---
+++ JS/Login.js
@@ -0,0 +1,5 @@
+const loginbutton = document.querySelector('#loginbutton');
+loginbutton.addEventListener('click', function () {
+  // Redirecionar para a página principal (Principal.html)
+  window.location.href = 'Tela-Principal.html';
+});
